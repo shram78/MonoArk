794f448823088a7418bbcb9e31f6ef769bbbc3cd
shram78 <ra00275@gmail.com>
2021-06-17 14:28:05 +0300

    внес изменения, хочу запушить в састер

--- MonoArk/GameEssence .cs
+++ MonoArk/GameEssence .cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+// внес изменения, хочу запушить в састер
 namespace MonoArk
 {
     abstract class GameEssence 
